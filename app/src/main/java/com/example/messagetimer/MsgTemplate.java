a2902c31e6815ef9017bdb47fea5a80ccb294b1e
psamsel <sp39427@zut.edu.pl>
2019-02-22 20:32:16 +0000

    Working on templates fragment - creating AlertDialog for adding template

--- app/src/main/java/com/example/messagetimer/MsgTemplate.java
+++ app/src/main/java/com/example/messagetimer/MsgTemplate.java
@@ -1,18 +1,19 @@
 package com.example.messagetimer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MsgTemplate
 {
     private String content;
 
-    public static final MsgTemplate[] templates =
-            {
-                    new MsgTemplate("Hey, wlasnie dojechalismy"),
-                    new MsgTemplate("Zostalo nam okolo pol godziny drogi")
-            };
+    public List<String> templates = new ArrayList<String>();
 
-    private MsgTemplate(String content)
+    public MsgTemplate()
     {
-        this.content = content;
+        templates.add("");
+        templates.add("Hey, wlasnie dojechalismy");
+        templates.add("Zostalo nam okolo pol godziny drogi");
     }
 
     public String getContent()
@@ -23,6 +24,16 @@ public class MsgTemplate
     public String toString()
     {
         return this.content;
+    }
+
+    public void addTemplate(String text)
+    {
+        templates.add(text);
+    }
+
+    public void deleteTemplate(long id)
+    {
+
     }
 
 }
--- app/src/main/java/com/example/messagetimer/TemplatesFragment.java
+++ app/src/main/java/com/example/messagetimer/TemplatesFragment.java
@@ -1,38 +1,54 @@
 package com.example.messagetimer;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class TemplatesFragment extends ListFragment implements AdapterView.OnItemClickListener
 {
     private long templateID;
+    private MsgTemplate templatesHandler = new MsgTemplate();
+    private View inflatedView;
+
+
+//dlg.show();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
-        String[] template = new String[MsgTemplate.templates.length];
-
-        for(int i = 0; i< template.length; i++)
-        {
-            template[i] = MsgTemplate.templates[i].getContent();
-        }
+        this.inflatedView = inflater.inflate(R.layout.fragment_templates, container, false);
+        Button button = (Button) inflatedView.findViewById(R.id.add_template);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
                 (
-                inflater.getContext(), android.R.layout.simple_list_item_1,
-                template
+                        inflater.getContext(),
+                        android.R.layout.simple_list_item_1,
+                        templatesHandler.templates
                 );
         setListAdapter(adapter);
 
-        return inflater.inflate(R.layout.fragment_templates, container, false);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //templatesHandler.addTemplate();
+            }
+
+        });
+
+        return this.inflatedView;
     }
 
 //    @Override
@@ -61,5 +77,31 @@ public class TemplatesFragment extends ListFragment implements AdapterView.OnIte
     {
         this.templateID = id;
     }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.add_template_dialog, null))
+                // Add action buttons
+                .setPositiveButton(R.string.confirm, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // sign in the user ...
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        TemplatesFragment.this.getDialog().cancel();
+                    }
+                });
+        return builder.create();
+    }
+
 
 }
